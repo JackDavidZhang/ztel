96ace516932e1e5c44940276acd918aa157e9e99
ztsubaki <2949125232@qq.com>
2024-11-12 22:08:08 +0800

    Add configure analyse

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
+mod config;
+
 use std::io::Read;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::net::{TcpListener,TcpStream};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let config = config::load_config();
+    let mut full_address = String::new();
+    full_address.push_str(&config.servers[0].address.as_str());
+    full_address.push_str(":");
+    full_address.push_str(&config.servers[0].port.to_string());
+    let listener = match TcpListener::bind(&full_address) {
+        Ok(listener) => listener,
+        Err(_) => panic!("unavailable address {}", full_address)
+    };
+    println!("Listening on {}", full_address);
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
-        let mut buffer = [0; 1024];
-        let len = stream.read(&mut buffer).unwrap();
+        println!("Connection from {} established!",stream.peer_addr().unwrap().to_string());
+        let mut buff = [0; 2048];
+        let len = stream.read(&mut buff).unwrap();
         for i in 0..len {
-            print!("{}", &buffer[i]);
+            print!("{} ", buff[i]);
         }
         println!();
     }
